cb510ee1dc7e09bc5105a0fb0dac5b994e50a911
MaxSter1 <63198631+MaxSter1@users.noreply.github.com>
2020-10-21 18:08:23 +0300

    Add files via upload

---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97f6f49b-8eed-4da9-b8bd-5ce43db912e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL;
+using DTO;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodCheckUser()
+        {
+            string connStr = "Data Source=COMPUTER-SQLEXPRESS02;Initial Catalog=KursovaPZ;Integrated Security=True";
+            ProductsDAL dal = new ProductsDAL(connStr);
+            ProductsDTO currentproduct = null;
+            currentproduct = dal.GetProductById(1);
+
+            Assert.AreEqual(currentproduct.Price, 100);
+        }
+
+        [TestMethod]
+        public void TestMethodCheckPriceUpdate()
+        {
+            string connStr = "Data Source=COMPUTER-SQLEXPRESS02;Initial Catalog=KursovaPZ;Integrated Security=True";
+            ProductsDAL dal = new ProductsDAL(connStr);
+            ProductsDTO currentproduct = null;
+            currentproduct = dal.GetProductById(4);
+            currentproduct.Price = 1000;
+            currentproduct = dal.UpdatePrice(currentproduct);
+
+            Assert.AreEqual(currentproduct.Price, 1000);
+        }
+
+
+        [TestMethod]
+        public void CheckProductsCount()
+        {
+            string connStr = "Data Source=COMPUTER-SQLEXPRESS02;Initial Catalog=KursovaPZ;Integrated Security=True";
+            ProductsDAL dal = new ProductsDAL(connStr);
+            int count = dal.GetAllProducts().Count;
+
+            Assert.AreEqual(count, 3);
+        }
+
+
+    }
+}
