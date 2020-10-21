f5b07d1db30cc90559a9728ccc9d05af5c7edd9e
MaxSter1 <63198631+MaxSter1@users.noreply.github.com>
2020-10-21 18:02:57 +0300

    Add files via upload

---
+++ DAL/IProductsDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public interface IProductsDAL
+    {
+        UserDTO CreateUser(UserDTO User);
+
+        List<ProductsDTO> GetAllProducts();
+        ProductsDTO UpdatePrice(ProductsDTO Product);
+
+        List<ProductsDTO> GetAllProductsSorted(int SortParameter);
+        ProductsDTO GetProductById(int Id);
+
+    }
+
+
+
+}
